6ef993625866003676c36ff092651e001970fa31
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-10 18:00:12 +0200

    added v-if on my html

--- js/script.js
+++ js/script.js
@@ -11,26 +11,31 @@ Far funzionare lo slider partendo dall’html ricevuto.
 
 const slides = [
     {
+        id: 1,
         image: 'img/01.jpg',
         title: 'Svezia',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     },
     {
+        id: 2,
         image: 'img/02.jpg',
         title: 'Svizzera',
         text: 'Lorem ipsum.',
     },
     {
+        id:3,
         image: 'img/03.jpg',
         title: 'Gran Bretagna',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     },
     {
+        id: 4,
         image: 'img/04.jpg',
         title: 'Germania',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
     },
     {
+        id: 5,
         image: 'img/05.jpg',
         title: 'Paradise',
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
@@ -54,11 +59,10 @@ new Vue (
         methods:{
             prevSlide(){
                 this.currentNumber++;
-
             },
             nextSlide(){
-                this.currentnumber--;
-                
+                this.currentNumber--;
+               
             },
         }
     }
